55dc4de0ef5fbf124029d66c068262955dde88d1
karolklos <karolklos1986@gmail.com>
2020-12-24 12:48:27 +0100

    Working on ShoppingListComponent, creating an Ingredient Model, creating and outputting the Shopping List, adding a ShoppingListEdit section

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,17 @@
+//Classic export
+
+// export class Ingredient {
+//   public name: string;
+//   public amount: number;
+
+//   constructor(name: string, amount: number) {
+//     this.name = name;
+//     this.amount = amount;
+//   }
+// } 
+
+//Shorter export
+
+export class Ingredient {
+  constructor(public name: string, public amount: number) {}
+}
